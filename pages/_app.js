d34f082aaaf6487075dc8f21953e72dac861d9ea
Pikkuherkko <juhhoh95@hotmail.com>
2023-01-16 23:08:12 +0200

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { StarknetConfig, InjectedConnector } from "@starknet-react/core";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  const connectors = [
+    new InjectedConnector({ options: { id: "braavos" } }),
+    new InjectedConnector({ options: { id: "argentX" } }),
+  ];
+  return (
+    <StarknetConfig connectors={connectors}>
+      <Component {...pageProps} />
+    </StarknetConfig>
+  );
 }
